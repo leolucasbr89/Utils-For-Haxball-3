1a7a50b933f17df24b20dfada0811767ffda9b24
leolucasbr89 <batalhas.beyblades.bol@gmail.com>
2024-04-22 13:12:39 -0300

    unis e type

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,2 +1,10 @@
 export type idioms = "en"|"pt"|"es"
 export type Position = {x: number, y: number}
+
+export type Team =  {
+    shirt: {angle: number, textColor: number, colors: ReadonlyArray<number>},
+    name: string,
+    cheerphrases: string[]
+    color: number,
+    regiao?: string
+}
